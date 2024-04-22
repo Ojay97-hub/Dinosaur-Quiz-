37259307fd312cffa5eda2ef4ae31d01603be4a9
Owen Cotter <owenjames97@outlook.com>
2024-04-22 17:19:33 +0000

    creating info.js file for the info.html button

---
+++ assets/javascript/info.js
@@ -0,0 +1,6 @@
+// putting script in its own file for the info page
+document
+  .getElementById("play-again-btn")
+  .addEventListener("click", function () {
+    window.location.href = "index.html"; // Redirect to the main quiz page
+  });
